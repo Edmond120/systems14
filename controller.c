46f6a08b76548546fb00889f66b898f0f2a4ed82
edmond120 <edmondwong888@msn.com>
2017-12-06 14:52:08 -0500

    done with one small bug

--- controller.c
+++ controller.c
@@ -2,33 +2,74 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-void flag_c(int argc,char ** argv){
-
+int key = 1234; // #bestkeyever
+int descriptor = -1;
+void flag_c(int argc,char ** argv){//creates a semaphore
+	if(argc < 2){
+		printf("not enough arguments\n");
+	}
+	int i;
+	for(i = 0; argv[2][i];i++){
+		if(!isdigit(argv[2][i])){
+			printf("%s is not a number\n",argv[2]);
+			return;
+		}
+	}
+	if(descriptor == -1){
+		descriptor = semget(key,1,IPC_CREAT);
+		semctl(descriptor,0,SETVAL,atoi(argv[2]));
+		printf("semaphore created: %d\n",descriptor);
+	}
+	else{
+		printf("semaphore is already created\n");
+		return;
+	}
 }
-void flag_v(int argc,char ** argv){
-
+void flag_v(){//views a semaphore
+	if(descriptor == -1){
+		printf("semaphore is not created yet\n");
+		return;
+	}
+	else{
+		printf("semaphore value: %d",semctl(descriptor,0,GETVAL));
+	}
 }
-void flag_r(int argc,char ** argv){
-
+void flag_r(){//removes a semaphore
+	if(descriptor == -1){
+		printf("no semaphore to remove\n");
+		return;
+	}
+	else{
+		if(semctl(descriptor,0,IPC_RMID) == 0){
+			printf("semaphore removed\n");
+			return;
+		}
+		else{
+			printf("error removing semaphore\n");
+		}
+	}
 }
 
 int main(int argc, char ** argv){
 	if(argc <= 1){
-		printf("usage:\n-c N\n    creates a semaphore and sets its value to N\n\n-v\n    view the current value of the semaphore\n\n-r\n    removes the semaphore\n");
+		printf("usage:\n-c <N>\n    creates a semaphore and sets its value to N\n\n-v\n    view the current value of the semaphore\n\n-r\n    removes the semaphore\n");
 		return 0;
 	}
 	if(!strcmp(argv[1],"-c")){
 		flag_c(argc,argv);
 	}
 	else if(!strcmp(argv[1],"-v")){
-		flag_v(argc,argv);
+		flag_v();
 	}
 	else if(!strcmp(argv[1],"-r")){
-		flag_r(argc,argv);
+		flag_r();
 	}
 	else{
-		printf("unknown flag: %s\n",argc[1]);
+		printf("unknown flag: %s\n",argv[1]);
 	}
 	return 0;
 }
