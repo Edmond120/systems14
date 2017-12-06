a13aa99ea8dbddd3a2d673cd46faaa511701448a
edmond120 <edmondwong888@msn.com>
2017-12-06 11:40:00 -0500

    flag catching

--- controller.c
+++ controller.c
@@ -8,4 +8,17 @@ int main(int argc, char ** argv){
 		printf("usage:\n-c N\n    creates a semaphore and sets its value to N\n\n-v\n    view the current value of the semaphore\n\n-r\n    removes the semaphore\n");
 		return 0;
 	}
+	if(!strcmp(argv[1],"-c")){
+
+	}
+	else if(!strcmp(argv[1],"-v")){
+
+	}
+	else if(!strcmp(argv[1],"-r")){
+
+	}
+	else{
+		printf("unknown flag: %s\n",argc[1]);
+	}
+	return 0;
 }
